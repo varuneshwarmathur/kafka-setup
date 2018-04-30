6fdc164e6dd318ce5768e667b91a33919a2ceeaa
Rishi Pratap Singh <rsingh24@sapient.com>
2018-04-30 16:12:46 +0100

    Adding files for kafka setup

---
+++ kafka-cluster.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cmd=${1}
+script=$(basename $0)
+projectname=kafkacluster
+kafka_image=spiside/kafka-cluster
+
+
+docker_compose() {
+    docker-compose -p $projectname "$@"
+}
+
+if [ -n "$cmd" ] && [ "$cmd" = 'setup' ]; then
+
+        # Check that the image exists and, if not, pull it.
+        docker inspect spiside/kafka-cluster &> /dev/null
+        if [ $? -ne 0 ]; then
+            echo "Docker image doesn't exist, pulling..."
+            docker pull $kafka_image
+        fi
+
+        set -e
+        # Start up the containers.
+        docker_compose up -d --force-recreate
+        docker_compose scale kafka=2
+        sleep 3  # wait for kafka to start.
+        echo "setup ran successfully!"
+        set +e
+
+fi
